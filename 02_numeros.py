6a91e74ecb2df2ae058811d438a075dbe9b1dc58
chico <franciscotamudo@gmail.com>
2024-08-20 18:52:51 +0200

    flotantes

---
+++ 02_numeros.py
@@ -0,0 +1,46 @@
+#boolenanos
+
+esta_abierta=True
+print(esta_abierta)
+
+hay_azucar=False
+print(hay_azucar)
+
+
+#enteros
+valor1=8
+print (valor1)
+valor1=-99
+print(valor1)
+
+#operadores
+
+valor1=10
+valor2=20
+print(valor1+valor2)
+print(valor1-valor2)
+print(valor1*valor2)
+print(valor2/valor1)
+print(valor2//valor1)
+print(valor2%valor1)
+print(valor2**valor1)
+
+#asignacion aumentada
+
+valor1=10
+valor2=valor1+5
+print(valor2)
+valor2 +=1
+print(valor2)
+valor2 -=1
+print(valor2)
+
+#valor abosoluto
+valor1=-10
+print(abs(valor1))
+
+#flotantes
+valor1=10
+valor2=-23.9
+print(valor1+valor2)
+
