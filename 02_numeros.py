884b127d7b90ad522d4dd4896baa07333de4613f
chico <franciscotamudo@gmail.com>
2024-08-20 19:00:03 +0200

    numeros

--- 02_numeros.py
+++ 02_numeros.py
@@ -44,3 +44,25 @@ valor1=10
 valor2=-23.9
 print(valor1+valor2)
 
+
+#convertir integer-float
+
+valor1=10
+print(float(valor1))
+valor1=10.0
+print(int(valor1))
+
+
+#type cual es el tipo de una variable dada
+
+valor1=23.3
+print(type(valor1))
+valor1=-23
+print(type(valor1))
+
+#redondear un flotante
+valor1=3.14162222
+print(round(valor1))
+print(round(valor1,2))
+
+      
