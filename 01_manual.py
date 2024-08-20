b4064832ab82945a039eeff2dfc55b912b196db3
chico <franciscotamudo@gmail.com>
2024-08-20 18:34:06 +0200

    inicio

---
+++ 01_manual.py
@@ -0,0 +1,66 @@
+
+
+# 01_COMENTARIOS
+#________________
+
+
+
+#este es el primer comentario
+#este es el segundo comentario
+
+"""
+este es un comentario de varias frases
+aqui puedes escribir lo que quieras
+s
+
+"""
+
+# 02_DATOS-VARIABLES-CONSTANTES
+#_____________________
+
+#booleanos
+
+casa=True
+chalet=False
+
+#enteros(int), flotantes(float),complejos(complex)
+
+ancho=19
+alto=24.5
+complejo=3+5j
+
+#texto(string)
+
+libro="en un lugar de la mancha"
+
+
+#tupla() es una estructura de datos inmutable no los podemos cambiar
+
+lista_compra=("manzanas","jamon","agua","chorizo")
+
+#listas[] es una estructura de datos mutable se  pueden borrar, actualizar, añadir..
+
+numeros_pares=[2,4,6,8,10]
+
+#diccionarios {} tal cual cada clave tiene un valor
+
+pobalcion_municipio={
+
+    2015:234,
+    2016:300,
+    2017:500,
+    2018:1000
+}
+
+#set o conjuntos valores unicos sin ordenar, son inmutables y sin orden establecido
+
+numeros_impares={1,3,5,11,7,9}
+
+#constantes
+
+VELOCIDAD_LUZ=3000000
+
+
+
+
+
