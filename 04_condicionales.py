2959dc239d81431dfeac4e0bf60bc867834f108a
chico <franciscotamudo@gmail.com>
2024-08-20 19:45:44 +0200

    condicionales

---
+++ 04_condicionales.py
@@ -0,0 +1,32 @@
+#primera condicional
+
+edad=30
+
+if edad>18:
+    print("eres mayor de edad")
+
+# segunda verdadero o falso
+edad=5
+if edad>18:
+    print("eres mauyor de edad")
+else:
+    print("eres menor de edad")
+
+# tercera condicional mas de una condicion
+
+if edad>10:
+    print("eres benjammin")
+elif edad>20:
+    print("eres juvenil")
+elif edad>30:
+    print('te queda poco para ser profesional')
+else:
+    print('no tienes edad para practicar el futbol eres un chupetin')
+
+#operadores comparacion == != < > <= >=
+#operadores logicos and or not
+edad=21
+if edad>20 and edad<30:
+    print('tienes edad de estar en la uni')
+
+#estructura mas avanzada match variable case
